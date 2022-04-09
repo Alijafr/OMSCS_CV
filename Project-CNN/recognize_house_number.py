21ce37e0aba2d53aa4286cc5eb7efa0509ee9fa3
Labuser <labuser@Labusers-MacBook-Air.local>
2022-04-09 23:08:17 +0300

    edit run.py to satisfy the instructions

--- Project-CNN/recognize_house_number.py
+++ Project-CNN/recognize_house_number.py
@@ -3,6 +3,10 @@ import numpy as np
 import torch.nn as nn
 import torchvision.transforms as transforms
 from train_models import apply_transforms
+from models import custom_model, VGG16
+import torch
+import sys
+import argparse
 
 
 def get_bboxes_MSER(img,expand=False,percentages = [0.2,-0.2]):
@@ -229,6 +233,35 @@ def read_house_numbers(image,model,use_cuda):
 # cv2.waitKey(0)
 
 # cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_type",default="custom_model",required=False)
+    parser.add_argument("--weights_file",default="custom_model.pt",required=False) #only for VGG16 
+    parser.add_argument("--input_image",default="dataset/train/format1/2.png",required=False) #name of the output image
+    parser.add_argument("--out_image",required=False) #name of the output image
+    args = parser.parse_args()
+    if args.model_type == "custom_model":    
+        model = custom_model(in_channels=3,num_classes= 11)
+        model.load_state_dict(torch.load(args.weights_file))
+    elif args.model_type == "VGG16":
+        model = VGG16(pretrained=False,in_channels=3,num_classes=11)
+        model.load_state_dict(torch.load(args.weights_file))
+    else:
+        print("wrong input for model_type, should be either custom_model or VGG16")
+        sys.exit()
+    
+    image_path =  args.input_image
+    image = cv2.imread(image_path)
+    use_cuda = torch.cuda.is_available()
+    vis = read_house_numbers(image, model, use_cuda)
+    if len(args.out_image):
+        cv2.imwrite("{}.png".format(args.out_image),vis)
+    cv2.imshow("vis",vis)
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
     
     
     
--- Project-CNN/run.py
+++ Project-CNN/run.py
@@ -2,20 +2,18 @@ from models import custom_model, VGG16
 import torch
 import sys
 import cv2
-import numpy
 import argparse
+import os
 from recognize_house_number import read_house_numbers
 
 
-
-
+test_folder = "./dataset/test_images/"
+out_folder = "./graded_images/"
 if __name__ == "__main__":
     
     parser = argparse.ArgumentParser()
     parser.add_argument("--model_type",default="custom_model",required=False)
     parser.add_argument("--weights_file",default="custom_model.pt",required=False) #only for VGG16 
-    parser.add_argument("--input_image",default="dataset/train/format1/2.png",required=False) #name of the output image
-    parser.add_argument("--out_image",required=False) #name of the output image
     args = parser.parse_args()
     if args.model_type == "custom_model":    
         model = custom_model(in_channels=3,num_classes= 11)
@@ -27,14 +25,12 @@ if __name__ == "__main__":
         print("wrong input for model_type, should be either custom_model or VGG16")
         sys.exit()
     
-    image_path =  args.input_image
-    image = cv2.imread(image_path)
-    use_cuda = torch.cuda.is_available()
-    vis = read_house_numbers(image, model, use_cuda)
-    if len(args.out_image):
-        cv2.imwrite("{}.png".format(args.out_image),vis)
-    cv2.imshow("vis",vis)
-    cv2.waitKey(0)
-
-    cv2.destroyAllWindows()
+    image_paths =  [f for f in os.listdir(test_folder) if os.path.isfile(os.path.join(test_folder, f))]
+    for i, path in enumerate(image_paths):
+        image = cv2.imread(path)
+        use_cuda = torch.cuda.is_available()
+        vis = read_house_numbers(image, model, use_cuda)
+        
+        cv2.imwrite("{}.png".format(args.test_folder+str(i)),vis)
+    
 
